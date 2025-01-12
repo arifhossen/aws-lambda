e3e948d4818af9494a8798f96041fb52919c282d
Arif Hossen <arifhossen2010@gmail.com>
2025-01-12 22:01:28 +0600

    New lambda source code
    New lambda source code

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement test
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!. push from github,yes got it, TEST Code....')
+        'body': json.dumps('New lambda code here by cicd ')
     }
